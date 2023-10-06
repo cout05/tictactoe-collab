5328016b751930a131ba26636bb887b61164bb51
Jerico <ecofranco1@gmail.com>
2023-10-06 15:45:04 +0800

    add the board and some game logic

---
+++ index.js
@@ -0,0 +1,16 @@
+const rows = document.getElementsByClassName("row"); // Get all elements that have the classname "row"
+
+let indicator = "X"; // stores the current value
+
+// Iterates all the rows variable and put an eventlistener on each of it
+for (let i = 0; i < rows.length; i++) {
+  rows[i].addEventListener("click", function () {
+    if (indicator === "O") {
+      rows[i].innerText = "X";
+      indicator = "X";
+    } else {
+      rows[i].innerText = "O";
+      indicator = "O";
+    }
+  });
+}
