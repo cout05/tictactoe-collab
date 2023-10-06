9bced351ee86e105cb9c11a134a5c4eaa8aee453
Jerico <ecofranco1@gmail.com>
2023-10-06 15:52:06 +0800

    fixed some bugs

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ let indicator = "X"; // stores the current value
 // Iterates all the rows variable and put an eventlistener on each of it
 for (let i = 0; i < rows.length; i++) {
   rows[i].addEventListener("click", function () {
-    if (indicator === "O") {
-      rows[i].innerText = "X";
-      indicator = "X";
-    } else {
-      rows[i].innerText = "O";
-      indicator = "O";
+    if (rows[i].innerText === "") {
+      if (indicator === "O") {
+        rows[i].innerText = "X";
+        indicator = "X";
+      } else {
+        rows[i].innerText = "O";
+        indicator = "O";
+      }
     }
   });
 }
