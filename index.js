adc177820753ffe3dc54f09a36e82b9389efdca6
Kevin <146004294+Mori2405@users.noreply.github.com>
2023-10-07 20:25:52 +0800

    Tic-tac-toe Functions(JS)
    Updated the Functions of Tic-tac-toe table in JavaScript

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
-const rows = document.getElementsByClassName("row"); // Get all elements that have the classname "row"
 
-let indicator = "X"; // stores the current value
+const cells = document.querySelectorAll('.row');
+const turnDisplay = document.getElementById('turn');
+const restartButton = document.getElementById('restartButton');
 
-// Iterates all the rows variable and put an eventlistener on each of it
-for (let i = 0; i < rows.length; i++) {
-  rows[i].addEventListener("click", function () {
-    if (rows[i].innerText === "") {
-      if (indicator === "O") {
-        rows[i].innerText = "X";
-        indicator = "X";
-      } else {
-        rows[i].innerText = "O";
-        indicator = "O";
-      }
+let game= 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+
+function handleCellClick(event) {
+    const clickedCell = event.target;
+    const cellIndex = clickedCell.getAttribute('data-cell-index');
+
+    if (gameBoard[cellIndex] !== '' || checkWinner(gameBoard)) {
+        return;
     }
-  });
+    game= game === "X" ? "O" : "X";
+    gameBoard[cellIndex] = game;
+    clickedCell.textContent = game;
+    if (checkWinner(gameBoard)) {
+        turnDisplay.textContent = `${currentPlayer} wins!`;
+    } else if (!gameBoard.includes('')) {
+        turnDisplay.textContent = "It's a draw!";
+    } else {
+        currentPlayer = currentPlayer === 'Player 1' ? 'Player 2' : 'Player 1';
+        turnDisplay.textContent = `${currentPlayer}'s turn`;
+    }
 }
+
+function checkWinner(board) {
+    const winningCombination = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let combination of winningCombination) {
+        const [a, b, c] = combination;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function restartGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = 'Player 1';
+    turnDisplay.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = '');
+}
+
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+restartButton.addEventListener('click', restartGame);
