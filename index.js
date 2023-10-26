5b46ad1965f805b9c46c7e9185762921e47fe5e6
Jerico <116884354+cout05@users.noreply.github.com>
2023-10-27 00:23:51 +0800

    updated the computers move

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const cells = document.querySelectorAll(".row");
 const turnDisplay = document.getElementById("turn");
+const modeInd = document.getElementById("mode-ind");
 
 let game = "X"; // Letter to input
 let gameBoard = ["", "", "", "", "", "", "", "", ""]; // Initial column spaces value
 let isComputerPlayer = false;
-let currentPlayer = "Player 1"; // Add currentPlayer variable to keep track of the current player
+let currentPlayer = true; // condition for players turn
 
 // choose mode
 const chooseModeContainer = document.getElementById("mode");
@@ -12,17 +13,29 @@ const boardContainer = document.getElementById("game");
 
 const gameMode = (param) => {
   chooseModeContainer.style.display = "none";
-  boardContainer.style.display = "block";
+  boardContainer.style.display = "flex";
   if (param === "ai") {
     isComputerPlayer = true;
+    modeInd.textContent = "Player Vs AI";
   } else {
+    modeInd.textContent = "Player Vs Player";
     isComputerPlayer = false;
   }
 };
 
+function restartGame() {
+  gameBoard = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = "Player 1";
+  turnDisplay.textContent = `${currentPlayer}'s turn`;
+  cells.forEach((cell) => (cell.textContent = ""));
+  cells.forEach((cell) => cell.classList.remove("x"));
+  cells.forEach((cell) => cell.classList.remove("o"));
+}
+
 const backToMenu = () => {
   chooseModeContainer.style.display = "block";
   boardContainer.style.display = "none";
+  restartGame();
 };
 
 function handleCellClick(event) {
@@ -33,29 +46,83 @@ function handleCellClick(event) {
     return;
   }
 
-  game = currentPlayer === "Player 1" ? "X" : "O";
+  game = currentPlayer ? "X" : "O";
   gameBoard[cellIndex] = game;
   clickedCell.textContent = game;
+  clickedCell.classList.add(`${game === "X" ? "x" : "o"}`);
 
   if (checkWinner(gameBoard)) {
-    turnDisplay.textContent = `${currentPlayer} wins!`;
+    turnDisplay.textContent = `Player${currentPlayer ? " 1" : " 2"} wins!`;
   } else if (!gameBoard.includes("")) {
     turnDisplay.textContent = "It's a draw!";
   } else {
-    currentPlayer = currentPlayer === "Player 1" ? "Player 2" : "Player 1";
-    turnDisplay.textContent = `${currentPlayer}'s turn`;
+    currentPlayer = currentPlayer ? false : true;
 
-    if (currentPlayer === "Player 2") {
+    if (isComputerPlayer) {
+      turnDisplay.textContent = `${
+        currentPlayer ? "Player 1" : "Computer"
+      }'s turn`;
+    } else {
+      turnDisplay.textContent = `Player${currentPlayer ? " 1" : " 2"}'s turn`;
+    }
+
+    if (!currentPlayer) {
       setTimeout(makeComputerMove, 1000); // Delay computer's move for 1 second
     }
   }
+}
+
+function checkPlayerMove(gameBoard) {
+  // Create a copy of the game board.
+  const boardCopy = [...gameBoard];
+
+  // Iterate over all empty squares on the board.
+  for (let i = 0; i <= 9; i++) {
+    if (boardCopy[i] === "") {
+      // Make the move on the copy of the board.
+      boardCopy[i] = "X";
+
+      // Check if the move would win the game for the player.
+      if (checkWinner(boardCopy, "X")) {
+        // returns an index for the move
+        return i;
+      }
+    }
+  }
+
+  // If no moves can be blocked, then return null.
+  return null;
+}
+
+function blockPlayerMove(gameBoard) {
+  // Create a copy of the game board.
+  const boardCopy = [...gameBoard];
+
+  // Iterate over all empty squares on the board.
+  for (let i = 0; i <= 9; i++) {
+    if (boardCopy[i] === "") {
+      // Make the move on the copy of the board.
+      boardCopy[i] = "O";
+
+      // Check if the move would win the game for the computer.
+      if (checkWinner(boardCopy, "O")) {
+        // returns an index for the move
+        return i;
+      }
+    }
+  }
+
+  // If no moves can be blocked, then return null.
+  return null;
 }
 
 function makeComputerMove() {
   if (!isComputerPlayer) {
     return; // Stop the function if computer player mode is disabled
   }
-  // Implement logic for the computer's move here
+
+  game = currentPlayer === "Player 1" ? "X" : "O";
+
   let emptyCells = gameBoard.reduce((acc, cell, index) => {
     if (cell === "") {
       acc.push(index);
@@ -63,27 +130,48 @@ function makeComputerMove() {
     return acc;
   }, []);
 
-  // Choose a random empty cell for the computer's move
-  const randomIndex = Math.floor(Math.random() * emptyCells.length);
-  const computerMoveIndex = emptyCells[randomIndex];
-
-  game = currentPlayer === "Player 1" ? "X" : "O";
-  gameBoard[computerMoveIndex] = game;
-  cells[computerMoveIndex].textContent = game;
+  if (emptyCells.length > 0) {
+    const blockMove = blockPlayerMove(gameBoard);
+    const checkMove = checkPlayerMove(gameBoard);
+    if (blockMove !== null) {
+      // Check if the player can win on their next move and block them
+      gameBoard[blockMove] = game;
+      cells[blockMove].textContent = game;
+      cells[blockMove].classList.add(`${game === "X" ? "x" : "o"}`);
+    } else if (checkMove !== null) {
+      // Checks if the computers move can win the game
+      gameBoard[checkMove] = game;
+      cells[checkMove].textContent = game;
+      cells[checkMove].classList.add(`${game === "X" ? "x" : "o"}`);
+    } else {
+      // If no immediate winning or blocking moves are available, make a random move
+      const randomIndex = Math.floor(Math.random() * emptyCells.length);
+      const computerMoveIndex = emptyCells[randomIndex];
+      game = currentPlayer === "Player 1" ? "X" : "O";
+      gameBoard[computerMoveIndex] = game;
+      cells[computerMoveIndex].textContent = game;
+      cells[computerMoveIndex].classList.add(`${game === "X" ? "x" : "o"}`);
+    }
+  }
 
   if (checkWinner(gameBoard)) {
-    turnDisplay.textContent = `${currentPlayer} wins!`;
+    turnDisplay.textContent = `${
+      currentPlayer && !isComputerPlayer ? "Player 1" : "Computer"
+    } wins!`;
     return; // stops the computer when it wins
   } else if (!gameBoard.includes("")) {
     turnDisplay.textContent = "It's a draw!";
   } else {
-    currentPlayer = currentPlayer === "Player 1" ? "Player 2" : "Player 1";
-    turnDisplay.textContent = `${currentPlayer}'s turn`;
+    currentPlayer = currentPlayer ? false : true;
+    turnDisplay.textContent = `${
+      currentPlayer ? "Player 1" : "Computer"
+    }'s turn`;
   }
-  if (isComputerPlayer && currentPlayer === "Player 2") {
+  if (isComputerPlayer && !currentPlayer) {
     setTimeout(makeComputerMove, 1000);
   }
 }
+
 // Function to check game winner
 function checkWinner(board) {
   const winningCombination = [
@@ -104,13 +192,6 @@ function checkWinner(board) {
     }
   }
   return false;
-}
-
-function restartGame() {
-  gameBoard = ["", "", "", "", "", "", "", "", ""];
-  currentPlayer = "Player 1";
-  turnDisplay.textContent = `${currentPlayer}'s turn`;
-  cells.forEach((cell) => (cell.textContent = ""));
 }
 
 // Puts an event listener to each of the cells that calls HandCellClick Function
